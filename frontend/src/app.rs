f261b493dc080c754764594ef8b48a08afca8be8
Flynn <FlynnOConnell@gmail.com>
2023-06-11 16:55:42 -0400

    feat: logos, landing page init

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,12 +1,12 @@
 use yew::prelude::*;
+use crate::landing::LandingPage;
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <main>
-            <img class="logo" src="https://yew.rs/img/logo.png" alt="Yew logo" />
-            <h1>{ "Hello World!" }</h1>
-            <span class="subtitle">{ "from Yew with " }<i class="heart" /></span>
-        </main>
+        <>
+            <LandingPage />
+        </>
     }
 }
+
