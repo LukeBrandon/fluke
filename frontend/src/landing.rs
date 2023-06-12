ec21fb3a1f3fc2fe5cc2beeff1ca9424254d4428
Flynn <75346097+NeuroPyPy@users.noreply.github.com>
2023-06-11 17:55:50 -0700

    feat: logos, landing page init (#12)
    Initialize a template for a UI, with logos, some nicer coloring and
    components.

---
+++ frontend/src/landing.rs
@@ -0,0 +1,51 @@
+// frontend/src/landing.rs
+use yew::prelude::*;
+
+#[function_component(LandingPage)]
+pub fn landing_page() -> Html {
+    html! {
+        <main>
+           <div id="outer">
+            <img id="logo" src="logo_transparent.png" alt="Logo" />
+                    <p id="links">{ "To Database" }</p>
+                   
+                    <p class="poem">
+                        {"In the land where silence and speech intersect,"}<br/>
+                        {"Lives a noble entity, Fluke, with a project."}<br/>
+                        {"Its mission pure, its resolve strong,"}<br/>
+                        {"To let voices thrive, where they belong."}<br/>
+                        <br/>
+                        {"With a whisper, a word, or a resonant shout,"}<br/>
+                        {"Fluke carries our secrets, there’s never a doubt."}<br/>
+                        {"An emissary silent, as it dances on air,"}<br/>
+                        {"To deliver our thoughts with exquisite care."}<br/>
+                        <br/>
+                        {"The world was once deaf, it couldn't hear,"}<br/>
+                        {"Behind a glass pane, each laugh, each tear."}<br/>
+                        {"But with the advent of Fluke, that wall is broken,"}<br/>
+                        {"Every heart can now speak, every word be spoken."}<br/>
+                        <br/>
+                        {"Encrypted messages, safe and sound,"}<br/>
+                        {"In Fluke’s embrace, trust is found."}<br/>
+                        {"Like a bird in flight, so swift and free,"}<br/>
+                        {"So is communication, as it's meant to be."}<br/>
+                        <br/>
+                        {"In the thrum of chats, in the hum of talks,"}<br/>
+                        {"Fluke stands its ground, like a silent ox."}<br/>
+                        {"It carries our whispers, to and fro,"}<br/>
+                        {"In the vast networks, where data flows."}<br/>
+                        <br/>
+                        {"In the sphere of privacy, a radiant knight,"}<br/>
+                        {"Against intrusion, Fluke will fight."}<br/>
+                        {"With its shield of code, its sword of tech,"}<br/>
+                        {"For the right to speak, it'll risk its neck."}<br/>
+                        <br/>
+                        {"So let's raise a toast, to this novel friend,"}<br/>
+                        {"An ally on whom, we can depend."}<br/>
+                        {"With a heart aflame and a privacy trend,"}<br/>
+                        {"Fluke, the future, to which we all commend."}<br/>
+                    </p>
+            </div>
+        </main>
+    }
+}
--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,7 +1,10 @@
 mod app;
+mod landing;
 
 use app::App;
 
 fn main() {
+    wasm_logger::init(wasm_logger::Config::default());
+    log::trace!("Initializing yew...");
     yew::Renderer::<App>::new().render();
 }
