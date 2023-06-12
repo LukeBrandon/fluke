b8b3516dce32a14bf0e04ab85734e1282cf5944b
Luke Brandon <37991252+LukeBrandon@users.noreply.github.com>
2023-06-11 20:11:04 -0500

    feat(messages): CRD on messages (#13)
    - Enabled CRD operation on `message` table:
      - Create new message
      - Read existing message
      - List existing messages
      - Delete existing message
      - NOT UPDATING yet
    - Updated `backend/README.md` to have useful setup information and
    migration information
    
    
    This was a bit of a struggle to get working, but this is what I cobbled
    together from both the `rocket` examples and the `sqlx` examples. There
    are still a few kinks that should be worked out to clean this up.

---
+++ backend/README.md
@@ -0,0 +1,21 @@
+# Fluke backend
+- Fluke's backend is written using `rocket.rs` and `sqlx`
+
+***
+
+### Environment Setup
+- To tell rocket where to find the database, set the following if a `.env` file
+  - `DATABASE_URL=postgres://<db_user>:<db_password>@<db_host>:<db_port>/<db_name>`
+    - This needs to be set for `sqlx` to know where to do migrations and also for `sqlx::query!()` and `sqlx::query_as!()` macros to function correctly
+      - These macros spin up test DB's and run sql against the DB to validate the SQL and give compiler errors on bad SQL
+    - The env var is used by `ROCKET_DATABASES` so that only 1 url is needed
+  - `ROCKET_DATABASES={fluke={url=${DATABASE_URL}}}`
+    - This needs to be set for `rocket.rs` to connect to the db
+  - This is loaded in with the help of the [`dotenv` create](https://docs.rs/dotenv/0.15.0/dotenv/fn.dotenv.html)
+
+### SQLx Migrations
+- Install `sqlx-cli` with `cargo install sqlx-cli`
+- To create a new migration: run `sqlx migrate add -r <migration-name>`
+  - Make sure the migration name is useful and each migration is focused to one thing
+- Write both a forward and reverse migration
+- For more infoo [see here](https://crates.io/crates/sqlx-cli)
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,45 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+
+use dotenv::dotenv;
+use rocket::{fairing, fairing::AdHoc, Build, Rocket};
+use rocket_db_pools::{sqlx, Database};
+
+mod messages;
+
+#[derive(Database)]
+#[database("fluke")]
+pub struct FlukeDb(sqlx::PgPool);
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Welcome to Fluke!"
+}
+
+/// This attempts to run migrations from the `./migrations` directory
+/// This was gotten from the rocket.rs sqlx example [here](https://github.com/SergioBenitez/Rocket/blob/v0.5-rc/examples/databases/src/sqlx.rs#L87)
+async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
+    match FlukeDb::fetch(&rocket) {
+        Some(db) => match sqlx::migrate!(".//migrations").run(&**db).await {
+            Ok(_) => Ok(rocket),
+            Err(e) => {
+                error!("Failed to initialize SQLx database: {}", e);
+                Err(rocket)
+            }
+        },
+        None => Err(rocket),
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    dotenv()
+        .ok()
+        .expect("Could not load environment variables from .env");
+
+    rocket::build()
+        .attach(FlukeDb::init())
+        .attach(AdHoc::try_on_ignite("SQLx Migrations", run_migrations))
+        .mount("/", routes![index])
+        .attach(messages::messages_stage())
 }
