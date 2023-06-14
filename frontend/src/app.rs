8f5399375230c3847626442aa2b484a4288beafd
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-14 02:14:57 -0400

    Add route switches with yew_router

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,13 +1,29 @@
-use crate::landing::LandingPage;
-use crate::header::Header;
 use yew::prelude::*;
+use yew_router::prelude::*;
+
+use crate::views::home::Home;
+
+#[derive(Clone, PartialEq, Routable)]
+pub enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+pub fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <Home /> },
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <>
-            <Header />
-            <LandingPage />
-        </>
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
     }
 }
