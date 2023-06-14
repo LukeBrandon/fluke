09a0d2c2f246818be90b882318983a04ccd6dcfe
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-14 17:09:29 -0400

    fix: form placeholder + callback for http req

--- frontend/src/views/home.rs
+++ frontend/src/views/home.rs
@@ -1,8 +1,8 @@
 use gloo_net::http::{Headers, Request};
 use serde::{Deserialize, Serialize};
-// use serde_json;
-// use wasm_bindgen::JsValue;
-// use web_sys::HtmlInputElement;
+use serde_json;
+use wasm_bindgen::JsValue;
+use web_sys::HtmlInputElement;
 
 use yew::prelude::*;
 
@@ -32,18 +32,85 @@ pub fn home() -> Html {
     let password_is_valid = use_state(|| true);
 
     log::info!("registration_form {:?}", &registration_form.clone());
-    // let onsubmit = { !todo(); };
+    let onsubmit = {
+        let registration_form = registration_form.clone();
+
+        let username = username.clone();
+        let first_name_ref = first_name_ref.clone();
+        let last_name_ref = last_name_ref.clone();
+        let email_ref = email_ref.clone();
+        let password_ref = password_ref.clone();
+        let confirm_password_ref = confirm_password_ref.clone();
+
+        let password_is_valid = password_is_valid.clone();
+
+        Callback::from(move |event: SubmitEvent| {
+            event.prevent_default();
+            log::info!("registration_form {:?}", &registration_form.clone());
+
+            let username = username.cast::<HtmlInputElement>().unwrap().value();
+            let first_name = first_name_ref.cast::<HtmlInputElement>().unwrap().value();
+            let last_name = last_name_ref.cast::<HtmlInputElement>().unwrap().value();
+            let email = email_ref.cast::<HtmlInputElement>().unwrap().value();
+            let password = password_ref.cast::<HtmlInputElement>().unwrap().value();
+            let confirm_password = confirm_password_ref
+                .cast::<HtmlInputElement>()
+                .unwrap()
+                .value();
+
+
+            // Fix: 
+            // Check password against DB entry -----            
+            if password != confirm_password {
+                password_is_valid.set(false);
+                return;
+            } else {
+                password_is_valid.set(true);
+            };
+
+            let registration_form = RegistrationForm {
+                username,
+                first_name,
+                last_name,
+                email,
+                password,
+                confirm_password,
+            };
+
+            log::info!("registration_form {:?}", &registration_form);
+
+            // see https://github.com/rustwasm/wasm-bindgen/issues/1607#issuecomment-503499362
+            wasm_bindgen_futures::spawn_local(async move {
+                let post_request = Request::post("https://reqres.in/api/register")
+                    .headers({
+                        let headers = Headers::new();
+                        headers
+                            // .append(name, value)
+                            .set("Content-Type", "application/json");
+                        headers
+                    })
+                    .body(JsValue::from(
+                        serde_json::to_string(&registration_form).unwrap(),
+                    ))
+                    .send()
+                    .await
+                    .unwrap();
+
+                log::info!("post_request {:?}", &post_request);
+            });
+        })
+    };
     html! {
         <main class="home">
             <h1>{"User Registration"}</h1>
-            // <form {onsubmit} class="registration-form"> # to do
-            <form class="registration-form">
-                <InputField input_node_ref={username} label={"Username".to_owned()} name={"username".clone()} field_type={"text".clone()} />
-                <InputField input_node_ref={first_name_ref} label={"First Name".to_owned()} name={"first_name".clone()} field_type={"text".clone()}  />
-                <InputField input_node_ref={last_name_ref} label={"Last Name".to_owned()} name={"last_name".clone()} field_type={"text".clone()}  />
-                <InputField input_node_ref={email_ref} label={"Email".to_owned()} name={"email".clone()} field_type={"email".clone()}  />
-                <InputField input_node_ref={password_ref} label={"Password".to_owned()} name={"password".clone()} field_type={"password".clone()}  />
-                <InputField input_node_ref={confirm_password_ref} label={"Confirm Password".to_owned()} name={"confirm_password".clone()} field_type={"password".clone()}  />
+            <form {onsubmit} class="registration-form">
+                <InputField input_node_ref={username} name={"username".clone()} field_type={"text".clone()} placeholder={"Username".clone()} />
+                <InputField input_node_ref={email_ref} name={"email".clone()} field_type={"email".clone()}  placeholder={"Email".clone()}/>
+                <InputField input_node_ref={first_name_ref} name={"first_name".clone()} field_type={"text".clone()} placeholder={"First name".clone()}  />
+                <InputField input_node_ref={last_name_ref}  name={"last_name".clone()} field_type={"text".clone()}  placeholder={"Last name".clone()}/>
+                // feat: validate that it is a valid email address
+                <InputField input_node_ref={password_ref} name={"password".clone()} field_type={"password".clone()}  placeholder={"Password".clone()}/>
+                <InputField input_node_ref={confirm_password_ref} name={"confirm_password".clone()} field_type={"password".clone()}  placeholder={"Retype password".clone()}/>
                 <p class="error-text">{ if *password_is_valid { "" } else { "Passwords do not match" } }</p>
                 <button type="submit" class="button button-primary">{"Submit"}</button>
             </form>
