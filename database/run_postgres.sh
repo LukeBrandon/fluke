dd3d12986828f672f0c4c468df3e2035e134e2ce
Flynn <oconnell@binghamton.edu>
2023-06-18 18:55:58 -0400

    add database folder for docker

---
+++ database/run_postgres.sh
@@ -0,0 +1,22 @@
+# run-postgres.sh
+set -e
+HOST_PORT=5432
+NAME=postgres-dev
+DOCKER_REPO=postgres
+TAG=14.1
+
+docker run --rm --name $NAME \
+  --volume `pwd`/pgdata:/var/lib/pgsql/data \
+  --volume `pwd`/mnt_data:/mnt/data \
+  --volume `pwd`/pg_hba.conf:/etc/postgresql/pg_hba.conf \
+  --volume `pwd`/postgresql.conf:/etc/postgresql/postgresql.conf \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_USER=postgres \
+  -e PGDATA=/var/lib/pgsql/data/pgdata14 \
+  -e POSTGRES_INITDB_ARGS="--data-checksums --encoding=UTF8" \
+  -e POSTGRES_DB=db \
+  -p ${HOST_PORT}:5432 \
+  ${DOCKER_REPO}:${TAG} \
+  postgres \
+    -c 'config_file=/etc/postgresql/postgresql.conf' \
+    -c 'hba_file=/etc/postgresql/pg_hba.conf'
