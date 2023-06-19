891808f34f83bafb714fe754b4cb46215f0291c6
Flynn <75346097+NeuroPyPy@users.noreply.github.com>
2023-06-19 18:59:01 -0400

    fix: unique email/username values with unique ID
    Co-authored-by: Luke Brandon <37991252+LukeBrandon@users.noreply.github.com>

--- backend/migrations/0003_create_users_table.up.sql
+++ backend/migrations/0003_create_users_table.up.sql
@@ -1,11 +1,10 @@
 -- create_users_table.up
 -- Prefer TEXT > VARCHAR: https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_varchar.28n.29_by_defaultj
 CREATE TABLE IF NOT EXISTS user(
-    id BIGSERIAL NOT NULL,
-    username TEXT NOT NULL,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
-    email TEXT NOT NULL,
+    email VARCHAR(254) NOT NULL UNIQUE,
     password TEXT NOT NULL,
-    PRIMARY KEY (username, email)
 );
