0bd341f33a010ea3f8c9fd437fb48c056570b69a
Flynn <75346097+NeuroPyPy@users.noreply.github.com>
2023-06-19 18:56:02 -0400

    fix: table names unpluralize
    Co-authored-by: Luke Brandon <37991252+LukeBrandon@users.noreply.github.com>

--- backend/migrations/0003_create_users_table.up.sql
+++ backend/migrations/0003_create_users_table.up.sql
@@ -1,6 +1,6 @@
 -- create_users_table.up
 -- Prefer TEXT > VARCHAR: https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_varchar.28n.29_by_defaultj
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS user(
     id BIGSERIAL NOT NULL,
     username TEXT NOT NULL,
     first_name TEXT NOT NULL,
