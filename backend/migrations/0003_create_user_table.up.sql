8cf508765cabfda28afdfb91f5e33ca76eb798f8
Flynn <oconnell@binghamton.edu>
2023-06-19 19:23:12 -0400

    fix: migration table name from users --> user

--- backend/migrations/0003_create_user_table.up.sql
+++ backend/migrations/0003_create_user_table.up.sql
@@ -1,5 +1,4 @@
 -- create_users_table.up
--- Prefer TEXT > VARCHAR: https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_varchar.28n.29_by_defaultj
 CREATE TABLE IF NOT EXISTS user(
     id BIGSERIAL NOT NULL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
