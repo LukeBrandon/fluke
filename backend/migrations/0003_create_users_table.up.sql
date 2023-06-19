101252b6916452b4b95b6cd170d8237b0df00e17
Flynn <oconnell@binghamton.edu>
2023-06-19 16:40:48 -0400

    fix: add UNIQUE and make room for messages

--- backend/migrations/0003_create_users_table.up.sql
+++ backend/migrations/0003_create_users_table.up.sql
@@ -1,10 +1,11 @@
 -- create_users_table.up
 -- Prefer TEXT > VARCHAR: https://wiki.postgresql.org/wiki/Don%27t_Do_This#Don.27t_use_varchar.28n.29_by_defaultj
 CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+    id BIGSERIAL NOT NULL,
     username TEXT NOT NULL,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email TEXT NOT NULL,
-    password TEXT NOT NULL
+    password TEXT NOT NULL,
+    PRIMARY KEY (username, email)
 );
