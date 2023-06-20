38f7eac891c1c6528d27e72c110b64f3809fc837
Flynn <oconnell@binghamton.edu>
2023-06-19 22:54:21 -0400

    fix: rename user table --> users (reserved psql name)

--- backend/migrations/0003_create_user_table.down.sql
+++ backend/migrations/0003_create_user_table.down.sql
@@ -1,2 +1,2 @@
 -- Add down migration script here
-DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS user_profile;
--- backend/migrations/0003_create_user_table.up.sql
+++ backend/migrations/0003_create_user_table.up.sql
@@ -1,9 +1,9 @@
 -- create_users_table.up
-CREATE TABLE IF NOT EXISTS user(
+CREATE TABLE IF NOT EXISTS user_profile(
     id BIGSERIAL NOT NULL PRIMARY KEY,
     username TEXT NOT NULL UNIQUE,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email VARCHAR(254) NOT NULL UNIQUE,
-    password TEXT NOT NULL,
+    password TEXT NOT NULL
 );
