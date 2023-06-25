94b235a22d26943208419972c4b5c96ebb5521b6
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-25 00:11:08 -0400

    api module, types and user_api functions

--- frontend/src/api/types.rs
+++ frontend/src/api/types.rs
@@ -1,17 +1,18 @@
-use chrono::prelude::*;
+// use chrono::prelude::*;
 use serde::{Deserialize, Serialize};
 
 #[allow(non_snake_case)]
 #[derive(Debug, Serialize, Deserialize, Default, PartialEq, Clone)]
 pub struct User {
     pub id: String,
-    pub name: String,
+    pub first_name: String,
+    pub last_name: String,
     pub email: String,
-    pub role: String,
-    pub photo: String,
-    pub verified: bool,
-    pub createdAt: DateTime<Utc>,
-    pub updatedAt: DateTime<Utc>,
+    pub is_admin: bool,
+    // pub photo: String,
+    // pub verified: bool,
+    // pub createdAt: DateTime<Utc>,
+    // pub updatedAt: DateTime<Utc>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
