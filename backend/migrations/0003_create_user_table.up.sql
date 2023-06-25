18f5c3a00d374e575ce9e3f71aef04a10f0868c2
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-25 13:36:40 -0400

    R8bfbdf5evert "remove username in users, migration"
    We don't want to break everything.
    
    This reverts commit 8bfbdf54560b0a11db6a93e22aa00e9c01375b44.

--- backend/migrations/0003_create_user_table.up.sql
+++ backend/migrations/0003_create_user_table.up.sql
@@ -1,6 +1,7 @@
 -- create_users_table.up
 CREATE TABLE IF NOT EXISTS fluke_user(
     id BIGSERIAL NOT NULL PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email VARCHAR(254) NOT NULL UNIQUE,
