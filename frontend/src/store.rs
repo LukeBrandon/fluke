f095f9b365f88e7bfc9634deef8471623d27b768
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-25 23:22:17 -0400

    user authentication to store

--- frontend/src/store.rs
+++ frontend/src/store.rs
@@ -8,16 +8,23 @@ use crate::api::types::User;
 pub struct Store {
     pub auth_user: Option<User>,
     pub page_loading: bool,
+    pub user_auth_error: String,
 }
 
 pub fn set_page_loading(loading: bool, dispatch: Dispatch<Store>) {
-    dispatch.reduce_mut(move |store| {
+    dispatch.reduce_mut(move |store: &mut Store| {
         store.page_loading = loading;
     })
 }
 
 pub fn set_auth_user(user: Option<User>, dispatch: Dispatch<Store>) {
-    dispatch.reduce_mut(move |store| {
+    dispatch.reduce_mut(move |store: &mut Store| {
         store.auth_user = user;
     })
+}
+
+pub fn set_user_auth_error(message: String, dispatch: Dispatch<Store>) {
+    dispatch.reduce_mut(move |store: &mut Store| {
+        store.user_auth_error = message;
+    });
 }
