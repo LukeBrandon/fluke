b0c89050cc805985a0fd8357da9664cd8e6ec7a7
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-25 23:21:57 -0400

    fix down migration for username drop

--- backend/migrations/0004_remove_username.down.sql
+++ backend/migrations/0004_remove_username.down.sql
@@ -1,2 +1,11 @@
 ALTER TABLE fluke_user
-ADD COLUMN username TEXT NOT NULL UNIQUE;
+ADD COLUMN username TEXT;
+
+UPDATE fluke_user
+SET username = 'user_' || id;
+
+ALTER TABLE fluke_user
+ALTER COLUMN username SET NOT NULL;
+
+ALTER TABLE fluke_user
+ADD CONSTRAINT username_unique UNIQUE (username);
