9450d1dab1c3284d45dcc1a662b67799787e2a1b
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-27 01:02:42 -0400

    new migration + discord color style

--- backend/migrations/0004_remove_username.down.sql
+++ backend/migrations/0004_remove_username.down.sql
@@ -1,11 +1,12 @@
-ALTER TABLE fluke_user
+ALTER TABLE fluke_user 
 ADD COLUMN username TEXT;
 
 UPDATE fluke_user
-SET username = 'user_' || id;
+SET username = split_part(email, '@', 1)
+WHERE username IS NULL;
 
-ALTER TABLE fluke_user
+ALTER TABLE fluke_user 
+ADD CONSTRAINT unique_username UNIQUE (username);
+
+ALTER TABLE fluke_user 
 ALTER COLUMN username SET NOT NULL;
-
-ALTER TABLE fluke_user
-ADD CONSTRAINT username_unique UNIQUE (username);
