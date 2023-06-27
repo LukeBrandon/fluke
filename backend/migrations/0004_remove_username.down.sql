a1625ce42be56d6fd69212b21fcf3796a889f2ab
Flynn OConnell <FlynnOConnell@gmail.com>
2023-06-27 01:08:35 -0400

    revert username drop migration with email as username

--- backend/migrations/0004_remove_username.down.sql
+++ backend/migrations/0004_remove_username.down.sql
@@ -2,7 +2,7 @@ ALTER TABLE fluke_user
 ADD COLUMN username TEXT;
 
 UPDATE fluke_user
-SET username = split_part(email, '@', 1)
+SET username = LOWER(email)
 WHERE username IS NULL;
 
 ALTER TABLE fluke_user 
