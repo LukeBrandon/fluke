5371751267c560e4eef803dc037f24ef4e7f1d51
Flynn <75346097+FlynnOConnell@users.noreply.github.com>
2023-08-05 22:02:44 -0400

    Update backend/migrations/20230804194015_add_user_to_message.up.sql
    Nope
    
    Co-authored-by: Luke Brandon <37991252+LukeBrandon@users.noreply.github.com>

--- backend/migrations/20230804194015_add_user_to_message.up.sql
+++ backend/migrations/20230804194015_add_user_to_message.up.sql
@@ -2,5 +2,5 @@
 ALTER TABLE message
 ADD COLUMN user_id BIGINT NOT NULL,
 ADD COLUMN channel_id BIGINT NOT NULL,
-ADD CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES fluke_user(id),
-ADD CONSTRAINT fk_channel FOREIGN KEY (channel_id) REFERENCES channel(id);
+ADD CONSTRAINT FOREIGN KEY (user_id) REFERENCES fluke_user(id),
+ADD CONSTRAINT FOREIGN KEY (channel_id) REFERENCES channel(id);
