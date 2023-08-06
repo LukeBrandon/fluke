9d2b53d81e19fe9257cb60ee13b10b99056363d5
Flynn <75346097+FlynnOConnell@users.noreply.github.com>
2023-08-06 15:09:22 -0400

    Remove DROP CONSTRAINT after dropping column
    Co-authored-by: Luke Brandon <37991252+LukeBrandon@users.noreply.github.com>

--- backend/migrations/20230804194015_add_user_to_message.down.sql
+++ backend/migrations/20230804194015_add_user_to_message.down.sql
@@ -1,6 +1,4 @@
 -- Add down migration script here
 ALTER TABLE message
-DROP CONSTRAINT IF EXISTS fk_user,
-DROP CONSTRAINT IF EXISTS fk_channel,
 DROP COLUMN IF EXISTS user_id,
 DROP COLUMN IF EXISTS channel_id;
