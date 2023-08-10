5ca56e4bb7a5bf84652eab98bf5d57f59a2c7a7a
Flynn OConnell <FlynnOConnell@gmail.com>
2023-08-10 00:01:27 -0400

    Revert "a"
    This reverts commit 0bf680b7fb1fbf19b28c28185a4e6d7f8cb906e2.

--- backend/src/controllers/message.rs
+++ backend/src/controllers/message.rs
@@ -25,7 +25,7 @@ pub async fn list_messages(Extension(pool): Extension<PgPool>) -> impl IntoRespo
 pub async fn get_message(
     Path(id): Path<i64>,
     Extension(pool): Extension<PgPool>,
-    ) -> Result<Json<MessageModel>, CustomError> {
+) -> Result<Json<MessageModel>, CustomError> {
     let sql = "SELECT * FROM message where id = ($1)";
 
     let message: MessageModel = sqlx::query_as::<_, MessageModel>(sql)
@@ -43,7 +43,7 @@ pub async fn get_message(
 pub async fn delete_message(
     Path(id): Path<i64>,
     Extension(pool): Extension<PgPool>,
-    ) -> Result<(StatusCode, Json<Value>), CustomError> {
+) -> Result<(StatusCode, Json<Value>), CustomError> {
     let sql = "DELETE FROM message WHERE id = ($1)";
 
     let _ = sqlx::query(sql)
@@ -62,19 +62,19 @@ pub async fn update_message(
     Path(id): Path<i64>,
     Extension(pool): Extension<PgPool>,
     Json(message): Json<CreateMessageSchema>,
-    ) -> Result<(StatusCode, Json<MessageModel>), CustomError> {
+) -> Result<(StatusCode, Json<MessageModel>), CustomError> {
     let updated = sqlx::query_as!(
         MessageModel,
         "UPDATE message SET message=$1 WHERE id=$2 RETURNING *",
         &message.message,
         id
-        )
-        .fetch_one(&pool)
-        .await
-        .map_err(|e| {
-            eprintln!("Database error: {}", e);
-            CustomError::InternalServerError
-        })?;
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|e| {
+        eprintln!("Database error: {}", e);
+        CustomError::InternalServerError
+    })?;
 
     Ok((StatusCode::OK, Json(updated)))
 }
@@ -82,7 +82,7 @@ pub async fn update_message(
 pub async fn create_message(
     Extension(pool): Extension<PgPool>,
     Json(message): Json<CreateMessageSchema>,
-    ) -> Result<(StatusCode, Json<MessageModel>), CustomError> {
+) -> Result<(StatusCode, Json<MessageModel>), CustomError> {
     if message.message.is_empty() {
         return Err(CustomError::BadRequest);
     }
@@ -91,14 +91,13 @@ pub async fn create_message(
         MessageModel,
         "INSERT INTO message (message, user_id, channel_id)  VALUES ($1, $2, $3) RETURNING *",
         &message.message, &message.user_id, &message.channel_id
-        )
-        .fetch_one(&pool)
-        .await
-        .map_err(|e| {
-            eprintln!("Database error: {}", e);
-            CustomError::InternalServerError
-        })?;
+    )
+    .fetch_one(&pool)
+    .await
+    .map_err(|e| {
+        eprintln!("Database error: {}", e);
+        CustomError::InternalServerError
+    })?;
 
     Ok((StatusCode::CREATED, Json(created)))
 }
-
