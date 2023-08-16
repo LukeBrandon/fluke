3d1d7a8d65afa3e1027e9e7e36ea515a77d99d0f
Flynn OConnell <FlynnOConnell@gmail.com>
2023-08-16 16:51:12 -0400

    rename to FlukeApiError, add 404

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -2,70 +2,75 @@ use axum::{
     http::StatusCode,
     response::{IntoResponse, Json, Response},
 };
+
 use serde_json::json;
 use sqlx::{error::DatabaseError, Error as SqlxError};
 
-pub enum CustomError {
+pub enum FlukeApiError {
     BadRequest(String),
+    NotFound(String),
+    Unauthorized(String),
     InternalServerError,
 }
 
-/// Converts `sqlx::Error` into `CustomError`.
+/// Converts `sqlx::Error` into `FlukeApiError`.
 ///
-/// Enables using the `?` operator in functions returning `Result<T, CustomError>`
+/// Enables using the `?` operator in functions returning `Result<T, FlukeApiError>`
 /// when interacting with functions that return `Result<T, sqlx::Error>`.
 ///
 /// # Example
 ///
 /// ```ignore
-/// fn my_function() -> Result<(), CustomError> {
+/// fn my_function() -> Result<(), FlukeApiError> {
 ///     another_function()?;  // This function returns `Result<(), sqlx::Error>`
 ///     Ok(())
 /// }
 /// ```
-impl From<SqlxError> for CustomError {
+impl From<SqlxError> for FlukeApiError {
     fn from(err: SqlxError) -> Self {
         match err {
+            SqlxError::RowNotFound => FlukeApiError::NotFound(err.to_string()),
             SqlxError::Database(db_error) => {
                 let pg_error = db_error.downcast::<sqlx::postgres::PgDatabaseError>();
                 if pg_error.is_unique_violation() {
-                    return CustomError::BadRequest(format!(
+                    return FlukeApiError::BadRequest(format!(
                         "Unique violation: {}",
                         pg_error.message()
                     ));
                 } else if pg_error.is_check_violation() {
-                    return CustomError::BadRequest(format!(
+                    return FlukeApiError::BadRequest(format!(
                         "Check violation: {}",
                         pg_error.message()
                     ));
                 } else if pg_error.is_foreign_key_violation() {
-                    return CustomError::BadRequest(format!(
+                    return FlukeApiError::BadRequest(format!(
                         "Foreign violation: {}",
                         pg_error.message()
                     ));
                 } else if pg_error.is_unique_violation() {
-                    return CustomError::BadRequest(format!(
+                    return FlukeApiError::BadRequest(format!(
                         "Duplicate key: {}",
                         pg_error.message()
                     ));
                 }
-                CustomError::BadRequest("A database error occurred".to_string())
+                FlukeApiError::InternalServerError // catch-all for database errors
             }
-            _ => CustomError::InternalServerError,
+            _ => FlukeApiError::InternalServerError, // catch-all for sqlx - non-db errors
+                                                     // https://docs.rs/sqlx/latest/sqlx/enum.Error.html
         }
     }
 }
 
-/// Converts `CustomError` into an HTTP `Response`.
+/// Converts `FlukeApiError` into an HTTP `Response`.
 ///
-/// Each `CustomError` variant is mapped to an appropriate HTTP status code and
+/// Each `FlukeApiError` variant is mapped to an appropriate HTTP status code and
 /// error message, ensuring consistent and meaningful error responses to clients.
 ///
 /// # Examples
 ///
 /// ```ignore
-/// // Given an instance of `CustomError::InternalServerError`:
-/// let error = CustomError::InternalServerError;
+/// // Given an instance of `FlukeApiError::InternalServerError`:
+/// let error = FlukeApiError::InternalServerError;
 /// let response = error.into_response();
 /// assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
 /// ```
@@ -73,14 +78,16 @@ impl From<SqlxError> for CustomError {
 /// ```ignore
 /// // For a database unique violation:
 /// let pg_error = sqlx::Error::Database(/*... simulated unique violation error ...*/);
-/// let custom_error = CustomError::from(pg_error);
+/// let custom_error = FlukeApiError::from(pg_error);
 /// let response = custom_error.into_response();
 /// assert_eq!(response.status(), StatusCode::BAD_REQUEST);
 /// ```
-impl IntoResponse for CustomError {
+impl IntoResponse for FlukeApiError {
     fn into_response(self) -> Response {
         let (status, error_message) = match self {
-            CustomError::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg),
+            FlukeApiError::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg),
+            FlukeApiError::NotFound(msg) => (StatusCode::NOT_FOUND, msg),
+            FlukeApiError::Unauthorized(msg) => (StatusCode::UNAUTHORIZED, msg),
             _ => (
                 StatusCode::INTERNAL_SERVER_ERROR,
                 "Internal Server Error".to_string(),
