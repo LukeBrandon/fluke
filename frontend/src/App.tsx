8fb53ec30e98eb9fdd06066e5ba89af4b0c733e8
Flynn OConnell <FlynnOConnell@gmail.com>
2023-08-20 05:12:24 -0400

    add header, better styling and dark theme

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,48 +1,56 @@
 import { useState } from 'react';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
 import Container from "@mui/material/Container";
-import MainContainer from './components/MainContainer';
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
 import MessageDisplay from "./components/MessageDisplay";
 import ChannelSelector from "./components/ChannelSelector";
 
-function Copyright() {
-    return (
-        <Typography variant="body2" color="text.secondary" align="center">
-            {"Copyright ©"}
-            <Link color="inherit" href="https://mui.com/">
-                Fluke
-            </Link>{" "}
-            {new Date().getFullYear()}.
-        </Typography>
-    );
-}
-
 export default function App() {
+    const darkTheme = createTheme({
+        palette: {
+            mode: 'dark',
+        },
+    });
+
     const [channelId, setChannelId] = useState<number | null>(null);
 
     const handleChannelSelect = (id: number) => {
         setChannelId(id);
     };
+
     return (
-        <Container maxWidth="sm">
-            <Box sx={{ my: 4 }}>
-                <MainContainer>
-                    <div>
-                        <h2>Channel Selector:</h2>
-                        <br></br>
-                        <ChannelSelector onChannelSelect={handleChannelSelect} />
-                        {channelId && (
-                            <>
-                                <h2>Messages from Channel {channelId}:</h2>
-                                <MessageDisplay url={`http://localhost:8000/channels/${channelId}/messages`} />
-                            </>
-                        )}
-                    </div>
-                </MainContainer>
-                <Copyright />
-            </Box>
-        </Container>
+        <ThemeProvider theme={darkTheme}>
+            <CssBaseline />
+            <Container maxWidth="md" sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center', // centers content horizontally
+                minHeight: '100vh', // ensures the container spans the entire height
+                paddingTop: '2rem', // provides some spacing at the top
+            }}>
+                {/* Header */}
+                <Box sx={{ mb: 5, width: '100%' }}>
+                    <Typography variant="h4" component="h1" align="center" gutterBottom>
+                        Fluke
+                    </Typography>
+                </Box>
+
+                {/* Channel Selector - centered and made wider */}
+                <Box sx={{ width: '80%', textAlign: 'center', mb: 3 }}>
+                    <Typography variant="h6">Channel Selector:</Typography>
+                    <ChannelSelector onChannelSelect={handleChannelSelect} />
+                </Box>
+
+                {/* Messages Display */}
+                {channelId && (
+                    <Box sx={{ width: '100%', mt: 3 }}>
+                        <Typography variant="h6" align="center">Messages from Channel {channelId}:</Typography>
+                        <MessageDisplay url={`http://localhost:8000/channels/${channelId}/messages`} />
+                    </Box>
+                )}
+            </Container>
+        </ThemeProvider>
     );
 }
