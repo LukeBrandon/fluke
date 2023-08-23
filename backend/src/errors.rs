3ea71d4f4ccf5221b56d4f78135f3f1728f8e29e
Flynn OConnell <FlynnOConnell@gmail.com>
2023-08-22 22:03:09 -0400

    more robust error handling

--- backend/src/errors.rs
+++ backend/src/errors.rs
@@ -1,103 +1,174 @@
-use axum::{
-    http::StatusCode,
-    response::{IntoResponse, Json, Response},
-};
+use axum::http::header::WWW_AUTHENTICATE;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use axum::Json;
+use sqlx::error::DatabaseError;
+use std::borrow::Cow;
+use std::collections::HashMap;
 
-use serde_json::json;
-use sqlx::{error::DatabaseError, Error as SqlxError};
-
+#[derive(thiserror::Error, Debug)]
 pub enum FlukeApiError {
-    BadRequest(String),
-    NotFound(String),
-    Unauthorized(String),
-    InternalServerError,
+    #[error("Bad Request")]
+    BadRequest,
+
+    #[error("authentication required")]
+    Unauthorized,
+
+    /// Return `403 Forbidden`
+    #[error("user may not perform that action")]
+    Forbidden,
+
+    /// Return `404 Not Found`
+    #[error("request path not found")]
+    NotFound,
+
+    /// Return `422 Unprocessable Entity`
+    ///
+    /// This also serializes the `errors` map to JSON to satisfy the requirement for
+    /// `422 Unprocessable Entity` errors in the Realworld spec:
+    /// https://realworld-docs.netlify.app/docs/specs/backend-specs/error-handling
+    #[error("error in the request body")]
+    UnprocessableEntity {
+        errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+    },
+
+    /// Automatically return `500 Internal Server FlukeApiError` on a `sqlx::FlukeApiError`.
+    ///
+    /// Via the generated `From<sqlx::FlukeApiError> for FlukeApiError` impl,
+    /// this allows using `?` on database calls in handler functions without a manual mapping step.
+    ///
+    /// That this could also contain database constraint errors, which should usually
+    /// be transformed into client errors (e.g. `422 Unprocessable Entity` or `409 Conflict`).
+    /// See `ResultExt` below for a convenient way to do this.
+    #[error("an error occurred with the database")]
+    Sqlx(#[from] sqlx::Error),
+
+    /// Return `500 Internal Server FlukeApiError` on a `anyhow::Error`.
+    ///
+    /// Via the generated `From<anyhow::Error> for FlukeApiError` impl, this allows the
+    /// use of `?` in handler functions to automatically convert `anyhow::Error` into a response.
+    ///
+    /// Like with `Sqlx::Error`, the actual error message is not returned to the client
+    /// for security reasons.
+    #[error("an internal server error occurred")]
+    Anyhow(#[from] anyhow::Error),
 }
 
-/// Converts `sqlx::Error` into `FlukeApiError`.
-///
-/// Enables using the `?` operator in functions returning `Result<T, FlukeApiError>`
-/// when interacting with functions that return `Result<T, sqlx::Error>`.
-///
-/// # Example
-///
-/// ```ignore
-/// fn my_function() -> Result<(), FlukeApiError> {
-///     another_function()?;  // This function returns `Result<(), sqlx::Error>`
-///     Ok(())
-/// }
-/// ```
-impl From<SqlxError> for FlukeApiError {
-    fn from(err: SqlxError) -> Self {
-        match err {
-            SqlxError::RowNotFound => FlukeApiError::NotFound(err.to_string()),
-            SqlxError::Database(db_error) => {
-                let pg_error = db_error.downcast::<sqlx::postgres::PgDatabaseError>();
-                if pg_error.is_unique_violation() {
-                    return FlukeApiError::BadRequest(format!(
-                        "Unique violation: {}",
-                        pg_error.message()
-                    ));
-                } else if pg_error.is_check_violation() {
-                    return FlukeApiError::BadRequest(format!(
-                        "Check violation: {}",
-                        pg_error.message()
-                    ));
-                } else if pg_error.is_foreign_key_violation() {
-                    return FlukeApiError::BadRequest(format!(
-                        "Foreign violation: {}",
-                        pg_error.message()
-                    ));
-                } else if pg_error.is_unique_violation() {
-                    return FlukeApiError::BadRequest(format!(
-                        "Duplicate key: {}",
-                        pg_error.message()
-                    ));
-                }
-                FlukeApiError::InternalServerError // catch-all for database errors
-            }
-            _ => FlukeApiError::InternalServerError, // catch-all for sqlx - non-db errors
-                                                     // https://docs.rs/sqlx/latest/sqlx/enum.Error.html
+impl FlukeApiError {
+    /// Convenient constructor for `FlukeApiError::UnprocessableEntity`.
+    ///
+    /// Multiple for the same key are collected into a list for that key.
+    pub fn unprocessable_entity<K, V>(errors: impl IntoIterator<Item = (K, V)>) -> Self
+    where
+        K: Into<Cow<'static, str>>,
+        V: Into<Cow<'static, str>>,
+    {
+        let mut error_map = HashMap::new();
+
+        for (key, val) in errors {
+            error_map
+                .entry(key.into())
+                .or_insert_with(Vec::new)
+                .push(val.into());
+        }
+
+        Self::UnprocessableEntity { errors: error_map }
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Self::BadRequest => StatusCode::BAD_REQUEST,
+            Self::Unauthorized => StatusCode::UNAUTHORIZED,
+            Self::Forbidden => StatusCode::FORBIDDEN,
+            Self::NotFound => StatusCode::NOT_FOUND,
+            Self::UnprocessableEntity { .. } => StatusCode::UNPROCESSABLE_ENTITY,
+            Self::Sqlx(_) | Self::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
 
-/// Converts `FlukeApiError` into an HTTP `Response`.
-///
-/// Each `FlukeApiError` variant is mapped to an appropriate HTTP status code and
-/// error message, ensuring consistent and meaningful error responses to clients.
-///
-/// # Examples
-///
-/// ```ignore
-/// // Given an instance of `FlukeApiError::InternalServerError`:
-/// let error = FlukeApiError::InternalServerError;
-/// let response = error.into_response();
-/// assert_eq!(response.status(), StatusCode::INTERNAL_SERVER_ERROR);
-/// ```
-///
-/// ```ignore
-/// // For a database unique violation:
-/// let pg_error = sqlx::Error::Database(/*... simulated unique violation error ...*/);
-/// let custom_error = FlukeApiError::from(pg_error);
-/// let response = custom_error.into_response();
-/// assert_eq!(response.status(), StatusCode::BAD_REQUEST);
-/// ```
 impl IntoResponse for FlukeApiError {
     fn into_response(self) -> Response {
-        let (status, error_message) = match self {
-            FlukeApiError::BadRequest(msg) => (StatusCode::BAD_REQUEST, msg),
-            FlukeApiError::NotFound(msg) => (StatusCode::NOT_FOUND, msg),
-            FlukeApiError::Unauthorized(msg) => (StatusCode::UNAUTHORIZED, msg),
-            _ => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                "Internal Server Error".to_string(),
-            ),
-        };
+        match self {
+            Self::UnprocessableEntity { errors } => {
+                #[derive(serde::Serialize)]
+                struct Errors {
+                    errors: HashMap<Cow<'static, str>, Vec<Cow<'static, str>>>,
+                }
 
-        let body = Json(json!({
-            "error": error_message,
-        }));
+                return (StatusCode::UNPROCESSABLE_ENTITY, Json(Errors { errors })).into_response();
+            }
+            Self::Unauthorized => {
+                return (
+                    self.status_code(),
+                    // Include the `WWW-Authenticate` challenge required in the specification
+                    // for the `401 Unauthorized` response code:
+                    // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/401
+                    [(WWW_AUTHENTICATE, "Token")],
+                    self.to_string(),
+                )
+                    .into_response();
+            }
 
-        (status, body).into_response()
+            Self::Sqlx(ref e) => {
+                // TODO: we probably want to use `tracing` instead
+                // so that this gets linked to the HTTP request by `TraceLayer`.
+                log::error!("SQLx error: {:?}", e);
+            }
+
+            Self::Anyhow(ref e) => {
+                // TODO: we probably want to use `tracing` instead
+                // so that this gets linked to the HTTP request by `TraceLayer`.
+                log::error!("Generic error: {:?}", e);
+            }
+
+            // Other errors get mapped normally.
+            _ => (),
+        }
+
+        (self.status_code(), self.to_string()).into_response()
+    }
+}
+
+/// A little helper trait for more easily converting database constraint errors into API errors.
+///
+/// ```rust,ignore
+/// let user_id = sqlx::query_scalar!(
+///     r#"insert into "user" (username, email, password_hash) values ($1, $2, $3) returning user_id"#,
+///     username,
+///     email,
+///     password_hash
+/// )
+///     .fetch_one(&ctxt.db)
+///     .await
+///     .on_constraint("user_username_key", |_| FlukeApiError::unprocessable_entity([("username", "already taken")]))?;
+/// ```
+pub trait ResultExt<T> {
+    /// If `self` contains a SQLx database constraint error with the given name,
+    /// transform the error.
+    ///
+    /// Otherwise, the result is passed through unchanged.
+    fn on_constraint(
+        self,
+        name: &str,
+        f: impl FnOnce(Box<dyn DatabaseError>) -> FlukeApiError,
+    ) -> Result<T, FlukeApiError>;
+}
+
+impl<T, E> ResultExt<T> for Result<T, E>
+where
+    E: Into<FlukeApiError>,
+{
+    fn on_constraint(
+        self,
+        name: &str,
+        map_err: impl FnOnce(Box<dyn DatabaseError>) -> FlukeApiError,
+    ) -> Result<T, FlukeApiError> {
+        self.map_err(|e| match e.into() {
+            FlukeApiError::Sqlx(sqlx::Error::Database(dbe)) if dbe.constraint() == Some(name) => {
+                map_err(dbe)
+            }
+            e => e,
+        })
     }
 }
