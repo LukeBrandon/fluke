fb3671f2dca0a169d83145c9c9a249dfe27991ce
Flynn OConnell <FlynnOConnell@gmail.com>
2023-08-22 22:20:47 -0400

    add HMAC key to .env for jwt auth

--- backend/src/configuration/mod.rs
+++ backend/src/configuration/mod.rs
@@ -1,27 +1,35 @@
-use crate::configuration::models::{FlukeConfiguration, FlukePort};
+use crate::configuration::models::{FlukeConfig, FlukePort};
 use dotenvy::dotenv;
 use std::env;
 
-mod models;
+pub mod models;
 
 /// Loads the database url from .evn file
-fn load_database_url() -> String {
+pub fn load_database_url() -> String {
     env::var("DATABASE_URL").expect("Mandatory DATABASE_URL was not provided")
 }
 
 /// Fetches the port number from env var PORT or None
-fn load_port() -> Option<FlukePort> {
+pub fn load_port() -> Option<FlukePort> {
     let env_port: String = env::var("PORT").ok()?;
     let port = env_port.parse::<u16>().ok()?;
     Some(FlukePort::from(port))
 }
 
+/// Fetches the port number from env var PORT or None
+pub fn load_hmac() -> String {
+    env::var("HMAC_KEY").expect(
+        "Mandatory HMAC_KEY was not provided, use 'openssl rand -base64 48' to generate one",
+    )
+}
+
 /// Loads from a .env file the configuration for Fluke
-pub fn load_config() -> FlukeConfiguration {
+pub fn load_config() -> FlukeConfig {
     dotenv().expect("No .env file provided.");
 
-    FlukeConfiguration {
+    FlukeConfig {
         database_url: load_database_url(),
         port: load_port().unwrap_or(FlukePort::default()),
+        hmac_key: load_hmac(),
     }
 }
--- backend/src/configuration/models.rs
+++ backend/src/configuration/models.rs
@@ -1,4 +1,6 @@
 const DEFAULT_PORT: u16 = 8000;
+
+#[derive(Clone)]
 pub struct FlukePort(pub u16);
 
 impl Default for FlukePort {
@@ -13,18 +15,21 @@ impl From<u16> for FlukePort {
     }
 }
 
-pub struct FlukeConfiguration {
+#[derive(Clone)]
+pub struct FlukeConfig {
     /// REQUIRED! There is not a default value
     pub database_url: String,
     /// Default 8000
     pub port: FlukePort,
+    pub hmac_key: String,
 }
 
-impl Default for FlukeConfiguration {
+impl Default for FlukeConfig {
     fn default() -> Self {
-        FlukeConfiguration {
+        FlukeConfig {
             database_url: "".to_string(),
             port: FlukePort::default(),
+            hmac_key: "".to_string(),
         }
     }
 }
